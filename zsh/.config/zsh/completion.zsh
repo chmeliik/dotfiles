18cfed9770a5b5639bd5f51039cc0d58b9b1f967
Adam Cmiel <acmiel@redhat.com>
2022-06-07 10:09:43 +0200

    zsh: completion cache, rehash automatically

--- zsh/.config/zsh/completion.zsh
+++ zsh/.config/zsh/completion.zsh
@@ -10,3 +10,8 @@ zstyle ':completion:*' matcher-list \
     'r:|?=** m:{[:lower:]}={[:upper:]}'
 
 zstyle ':completion:*:*:*:*:*' menu select
+
+zstyle ':completion:*' use-cache true
+zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh"
+
+zstyle ':completion:*' rehash true  # automatically update PATH entries
