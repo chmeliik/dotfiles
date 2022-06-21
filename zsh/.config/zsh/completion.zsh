f953f0f799c0db094e833236184d84b7c4d1f5c7
Adam Cmiel <acmiel@redhat.com>
2022-06-21 09:13:05 +0200

    zsh: try substring completion before fuzzy

--- zsh/.config/zsh/completion.zsh
+++ zsh/.config/zsh/completion.zsh
@@ -3,10 +3,11 @@ setopt complete_in_word
 # match files with leading dots
 _comp_options+=(glob_dots)
 
-# basic -> smart-case -> fuzzy smart-case
+# basic -> smart-case -> substring smart-case -> fuzzy smart-case
 zstyle ':completion:*' matcher-list \
     '' \
     'm:{[:lower:]}={[:upper:]}' \
+    'l:|=* r:|=* m:{[:lower:]}={[:upper:]}' \
     'r:|?=** m:{[:lower:]}={[:upper:]}'
 
 zstyle ':completion:*:*:*:*:*' menu select
