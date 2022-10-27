b1a1c6ed523fa6cc5df2343b2db6e29600ef9de2
Adam Cmiel <acmiel@redhat.com>
2022-10-27 14:34:12 +0200

    neovim: enable markdownlint formatting

--- nvim/.config/nvim/lua/user/lsp.lua
+++ nvim/.config/nvim/lua/user/lsp.lua
@@ -69,6 +69,7 @@ null_ls.setup({
   sources = {
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.formatting.black,
+    null_ls.builtins.formatting.markdownlint,
     null_ls.builtins.diagnostics.markdownlint,
     null_ls.builtins.diagnostics.write_good,
   },
