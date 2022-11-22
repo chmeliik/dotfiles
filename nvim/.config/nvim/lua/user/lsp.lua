a796a814dcf8d09d27de28c281c2ed059f892ed2
Adam Cmiel <acmiel@redhat.com>
2022-11-22 12:06:47 +0100

    neovim: add isort formatter for Python
    Signed-off-by: Adam Cmiel <acmiel@redhat.com>

--- nvim/.config/nvim/lua/user/lsp.lua
+++ nvim/.config/nvim/lua/user/lsp.lua
@@ -68,6 +68,7 @@ null_ls.setup({
   sources = {
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.formatting.black,
+    null_ls.builtins.formatting.isort,
     null_ls.builtins.formatting.markdownlint,
     null_ls.builtins.diagnostics.markdownlint,
     null_ls.builtins.diagnostics.write_good,
