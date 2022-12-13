df7a8c0e09f8c636001a975d6504efa803b9ddac
Adam Cmiel <acmiel@redhat.com>
2022-12-13 16:38:27 +0100

    zsh: add GOPROXY variable

--- zsh/.config/zsh/env.zsh
+++ zsh/.config/zsh/env.zsh
@@ -2,3 +2,6 @@ export path=("$HOME/.local/bin" $path)
 
 # run podman-docker as user
 export DOCKER_HOST="unix://$XDG_RUNTIME_DIR/podman/podman.sock"
+
+# speed up Go module downloads
+export GOPROXY="https://proxy.golang.org,direct"
