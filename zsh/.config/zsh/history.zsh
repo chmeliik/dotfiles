764e70c6a671bdeabfa043c4edba974567f12877
Adam Cmiel <acmiel1@gmail.com>
2023-03-12 16:12:36 +0100

    zsh history: ignore all duplicates
    Signed-off-by: Adam Cmiel <acmiel1@gmail.com>

--- zsh/.config/zsh/history.zsh
+++ zsh/.config/zsh/history.zsh
@@ -4,6 +4,6 @@ export SAVEHIST=$HISTSIZE
 
 setopt extended_history         # record timestamps and execution time
 setopt inc_append_history_time  # incrementally append with correct execution time
-setopt hist_ignore_dups         # ignore consecutive duplicates
+setopt hist_ignore_all_dups     # ignore all duplicates
 setopt hist_ignore_space        # don't save commands prefixed with whitespace
 setopt hist_verify              # show command before executing it
