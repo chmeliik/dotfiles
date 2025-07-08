05e34039f59b4b43cee22a4fa149a028016a83bc
Adam Cmiel <acmiel@redhat.com>
2025-07-08 17:56:42 +0200

    1

--- nvim/.config/nvim/lua/user/keymap.lua
+++ nvim/.config/nvim/lua/user/keymap.lua
@@ -10,6 +10,6 @@ map("n", "<C-k>", ":bprev<CR>")
 map("n", "<Leader>E", ":Lexplore 20<CR>")
 
 map("n", "<Leader>e", vim.diagnostic.open_float)
-map("n", "[d", mkfn(vim.diagnostic.jump, { count = 1, float = true }))
-map("n", "]d", mkfn(vim.diagnostic.jump, { count = -1, float = true }))
+map("n", "[d", mkfn(vim.diagnostic.jump, { count = -1, float = true }))
+map("n", "]d", mkfn(vim.diagnostic.jump, { count = 1, float = true }))
 map("n", "<Leader>q", vim.diagnostic.setloclist)
