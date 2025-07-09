cc2d2ee01b7560afe30c4a724744a46605ca3d38
Adam Cmiel <acmiel@redhat.com>
2025-07-09 08:13:11 +0200

    neovim: get rid of unused lib methods
    try() was completely unused
    mkfn() was only used in one place and unnecessary
    
    Signed-off-by: Adam Cmiel <acmiel@redhat.com>

--- nvim/.config/nvim/lua/user/keymap.lua
+++ nvim/.config/nvim/lua/user/keymap.lua
@@ -1,5 +1,4 @@
 local map = require("user.lib").map
-local mkfn = require("user.lib").mkfn
 
 -- use <Space> as leader key
 vim.g.mapleader = " "
@@ -10,8 +9,12 @@ map("n", "<C-k>", ":bprev<CR>")
 map("n", "<Leader>E", ":Lexplore 20<CR>")
 
 map("n", "<Leader>e", vim.diagnostic.open_float)
-map("n", "[d", mkfn(vim.diagnostic.jump, { count = -1, float = true }))
-map("n", "]d", mkfn(vim.diagnostic.jump, { count = 1, float = true }))
+map("n", "[d", function()
+  vim.diagnostic.jump({ count = -1, float = true })
+end)
+map("n", "]d", function()
+  vim.diagnostic.jump({ count = 1, float = true })
+end)
 map("n", "<Leader>q", vim.diagnostic.setloclist)
 
 map("n", "<A-k>", vim.lsp.buf.signature_help)
--- nvim/.config/nvim/lua/user/lib/init.lua
+++ nvim/.config/nvim/lua/user/lib/init.lua
@@ -7,22 +7,4 @@ function lib.map(mode, lhs, rhs)
   vim.keymap.set(mode, lhs, rhs, { silent = true })
 end
 
----@param fn function
----@param ... any
-function lib.try(fn, ...)
-  local ok, err = pcall(fn, ...)
-  if not ok then
-    vim.notify(err, vim.log.levels.WARN)
-  end
-end
-
----@param fn function
----@param ... any
-function lib.mkfn(fn, ...)
-  local args = { ... }
-  return function()
-    fn(table.unpack(args))
-  end
-end
-
 return lib
