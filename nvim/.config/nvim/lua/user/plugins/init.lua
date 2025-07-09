e9de3bd282f991664dc0148f3311eddf65b7a700
Adam Cmiel <acmiel@redhat.com>
2025-07-09 21:04:56 +0200

    neovim: move markdown plugins to separate file
    Signed-off-by: Adam Cmiel <acmiel@redhat.com>

--- nvim/.config/nvim/lua/user/plugins/init.lua
+++ nvim/.config/nvim/lua/user/plugins/init.lua
@@ -6,24 +6,6 @@ return {
   "stevearc/dressing.nvim",
 
   {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function()
-      vim.fn["mkdp#util#install"]()
-    end,
-  },
-
-  {
-    "MeanderingProgrammer/render-markdown.nvim",
-    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
-    ft = { "markdown", "codecompanion" },
-    ---@module 'render-markdown'
-    ---@type render.md.UserConfig
-    opts = {},
-  },
-
-  {
     "echasnovski/mini.align",
     version = "*",
     config = function()
---
+++ nvim/.config/nvim/lua/user/plugins/markdown.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
+
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+    ft = { "markdown", "codecompanion" },
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
+}
